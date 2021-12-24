0e3688bfb8f087462928440479e9095a74583326
Saikrishna Akkenapelly <saikrishna.akkenapelly@gmail.com>
2021-12-24 21:06:08 +0530

    registered with eureka server

--- src/main/java/com/sai/redirect/service/RedirectServiceApplication.java
+++ src/main/java/com/sai/redirect/service/RedirectServiceApplication.java
@@ -2,8 +2,10 @@ package com.sai.redirect.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RedirectServiceApplication
 {
 	public static void main(String[] args)
